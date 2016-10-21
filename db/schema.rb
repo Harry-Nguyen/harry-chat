7871bca03724f2ffa7f8f9542b8f55823b4a3e0c
Harry Nguyen <hamxvela@gmail.com>
2016-10-21 21:30:35 +0700

    Add friendship model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021131127) do
+ActiveRecord::Schema.define(version: 20161021142846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "owner_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_friendships_on_owner_id", using: :btree
+  end
 
   create_table "messages", force: :cascade do |t|
     t.integer  "sender_id"
