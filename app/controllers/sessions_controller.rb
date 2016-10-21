1333321106d5a9ce08cff881495ad46dc53b7761
Harry Nguyen <hamxvela@gmail.com>
2016-10-21 18:18:43 +0700

    User can login, logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+
+        flash.now[:success] = 'Login successfully.'
+        redirect_to root_path
+      else
+        flash.now[:error] = 'Incorrect password'
+        render 'new'
+      end
+    else
+      flash.now[:error] = 'User not found.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    
+    flash[:success] = 'Logout successfully.'
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users
+
+  get 'register' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login_submit' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  #delete 'sessions/destroy', as: :logout
 end
